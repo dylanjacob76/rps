7ab43b9c89cdafb5fa36093f5233106443449b59
Miyamoto Musashi <business.dmjacob@gmail.com>
2023-05-31 11:19:47 -0400

    completed ui

--- script.js
+++ script.js
@@ -1,55 +1,104 @@
+const containerEl = document.querySelector("#container");
+const results = document.createElement("div");
+const rockBtn = document.createElement("button");
+const paperBtn = document.createElement("button");
+const scissorsBtn = document.createElement("button");
+const playAgainBtn = document.createElement("button");
+const playerChoiceBtns = [rockBtn, paperBtn, scissorsBtn];
+
+function init() {
+  rockBtn.textContent = "Rock";
+  paperBtn.textContent = "Paper";
+  scissorsBtn.textContent = "Scissors";
+  containerEl.appendChild(results);
+  containerEl.setAttribute("style", "display: flex; flex-direction: column; justify-content: center; align-items: center;");
+  results.setAttribute("style", "margin-bottom: 20px; font-size: 24px")
+
+  playerChoiceBtns.forEach(button => {
+    containerEl.appendChild(button);
+    button.setAttribute("style", "margin: 4px; border-style: none; background-color: lightBlue; padding: 4px; width: 75px;");
+  });
+
+  startGame();
+}
+
 function getComputerChoice() {
-  const choices = ["rock+", "paper", "scissors"];
+  const choices = ["rock", "paper", "scissors"];
   const randomChoice = Math.floor(Math.random() * choices.length);
   return choices[randomChoice];
 }
 
-function game() {
-  let playerWins = 0, playerLosses = 0, playerTies = 0;
-  let computerWins = 0, computerLosses = 0, computerTies = 0;
-
-  function playRound(playerChoice, computerChoice = getComputerChoice()) {
-    playerChoice = prompt("Enter Rock, Paper, or Scissors:").toLowerCase();
-    if (playerChoice == computerChoice) {
-      console.log("Player Choice: " + playerChoice);
-      console.log("Computer Choice: " + computerChoice);
-      playerTies++;
-      computerTies++;
-      alert("This round's a tie!");
-    } else if (playerChoice === "rock" && computerChoice === "scissors" || playerChoice === "paper" && computerChoice === "rock" || playerChoice === "scissors" && computerChoice === "paper") {
-      console.log("Player Choice: " + playerChoice);
-      console.log("Computer Choice: " + computerChoice);
-      playerWins++;
-      computerLosses++;
-      alert("You won this round!");
-    } else {
-      console.log("Player Choice: " + playerChoice);
-      console.log("Computer Choice: " + computerChoice);
-      playerLosses++
-      computerWins++
-      alert("You lost this round.");
-    }
-  }
-
-  function loopRounds() {
-    for (let i = 0; i < 5; i++) {
-      playRound();
-    }
-  }
-
-  loopRounds();
-
-  function calculateWinner() {
-    if (playerWins === computerWins) {
-      alert("There is no winner, The games a draw!")
-    } else if (playerWins > computerWins) {
-      alert("You won the game!")
-    } else {
-      alert("GAME OVER \n \n Computer: I have bested you at this game pathetic human... \n \n Player: ... ... \n \n Computer: Rock, Paper, Scissor Supremacy IS MINE!")
-    }
-  }
-
-  calculateWinner();
+function playAgain() {
+  playAgainBtn.textContent = "Play Again";
+  playAgainBtn.setAttribute("style", "margin: 4px; border-style: none; background-color: lightGreen; font-weight: bold; padding: 4px;")
+  containerEl.appendChild(playAgainBtn);
+  playAgainBtn.addEventListener("click", startGame);
 }
 
-game();
+function startGame() {
+  playAgainBtn.setAttribute("style", "display: none;");
+  rockBtn.removeAttribute("disabled");
+  paperBtn.removeAttribute("disabled");
+  scissorsBtn.removeAttribute("disabled");
+  let playerWins = 0, playerLosses = 0, playerTies = 0;
+  let computerWins = 0, computerLosses = 0, computerTies = 0;
+  results.textContent = `Player Wins: ${playerWins} - Player Losses: ${playerLosses} - Player Ties: ${playerTies}`
+
+  function playerSelection() {
+    rockBtn.addEventListener("click", function () {
+      playRound("rock");
+    });
+    paperBtn.addEventListener("click", function () {
+      playRound("paper");
+    });
+    scissorsBtn.addEventListener("click", function () {
+      playRound("scissors");
+    });
+  }
+
+  playerSelection();
+
+  function playRound(playerChoice, computerChoice = getComputerChoice()) {
+
+    function declareWinner() {
+      if (playerWins === 5) {
+        results.textContent = "You Won the Game! The evil machines have been vanquished!";
+        endGame();
+      } else if (playerLosses === 5) {
+        results.textContent = "You Lost. The machines have won and declare world domination!";
+        endGame();
+      } else if (playerTies === 5) {
+        results.textContent = "it's a Tie! How boring...";
+        endGame();
+      }
+    }
+
+    function endGame() {
+      rockBtn.setAttribute("disabled", 1);
+      paperBtn.setAttribute("disabled", 1);
+      scissorsBtn.setAttribute("disabled", 1);
+      playAgain();
+    }
+
+    if (playerChoice == computerChoice) {
+      playerTies++;
+      computerTies++;
+    } else if (
+      playerChoice === "rock" && computerChoice === "scissors" ||
+      playerChoice === "paper" && computerChoice === "rock" ||
+      playerChoice === "scissors" && computerChoice === "paper"
+    ) {
+      playerWins++;
+      computerLosses++;
+    } else {
+      playerLosses++
+      computerWins++
+    }
+
+    results.textContent = `Player Wins: ${playerWins} || Player Losses: ${playerLosses} || Player Ties: ${playerTies}`;
+
+    declareWinner();
+  }
+}
+
+init();
